79b448ae131168211c889ca7c9f37a3649268269
“Hunter <hunterlmcdaniel@ufl.edu>
2020-11-08 22:54:04 -0500

    add instant js

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 // It is always helpful to use comments in your code!
+
+//212 in boiling temp span
+var boil;
+boil = () => "212";
+
+document.getElementById("boiling-temp").innerHTML = boil();
+
+//32 in freezing temp span
+var freeze;
+freeze = () => "32";
+
+document.getElementById("freezing-temp").innerHTML = freeze();
+
+//random number in water temp span
+var current;
+current = () => "58";
+
+document.getElementById("water-temp").innerHTML = current();
+
+//set timeout attempt
+setTimeout( function() {
+  if (current > 212){
+    document.getElementById("boiling").classList.remove("hide");
+    document.getElementById("boiling").classList.add("show");
+  }
+}, 3000)
