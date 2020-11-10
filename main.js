eaa8a8f157925dbfc1eeb9bd24cc04f10a7b50c7
“Hunter <hunterlmcdaniel@ufl.edu>
2020-11-10 10:56:48 -0500

    add final setTimeout

--- main.js
+++ main.js
@@ -2,26 +2,42 @@
 
 //212 in boiling temp span
 var boil;
-boil = () => "212";
+boil = 212;
 
-document.getElementById("boiling-temp").innerHTML = boil();
+document.getElementById("boiling-temp").innerHTML = boil;
 
 //32 in freezing temp span
 var freeze;
-freeze = () => "32";
+freeze = 32;
 
-document.getElementById("freezing-temp").innerHTML = freeze();
+document.getElementById("freezing-temp").innerHTML = freeze;
 
 //random number in water temp span
 var current;
-current = () => "58";
+current = 58;
 
-document.getElementById("water-temp").innerHTML = current();
+document.getElementById("water-temp").innerHTML = current;
 
 //set timeout attempt
 setTimeout( function() {
   if (current > 212){
     document.getElementById("boiling").classList.remove("hide");
     document.getElementById("boiling").classList.add("show");
+  }
+}, 3000)
+
+//set timeout attempt
+setTimeout( function() {
+  if (current < 32){
+    document.getElementById("frozen").classList.remove("hide");
+    document.getElementById("frozen").classList.add("show");
+  }
+}, 3000)
+
+//set timeout attempt
+setTimeout( function() {
+  if (current < 212 && current > 32){
+    document.getElementById("good-temp").classList.remove("hide");
+    document.getElementById("good-temp").classList.add("show");
   }
 }, 3000)
